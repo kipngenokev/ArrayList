9a0406c9057c3175a4ac7e4589171c15008362cf
brian070069 <kkipngeno409@gmail.com>
2025-02-02 23:56:24 +0300

    first commit on arrayList

---
+++ src/dev/lpa/MoreLists.java
@@ -0,0 +1,21 @@
+package dev.lpa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoreLists {
+    public static void main(String[] args) {
+        String[] items ={"apples", "bananas", "milk", "eggs"};
+
+        List<String> list = List.of(items);
+        System.out.println(list);
+
+        System.out.println(list.getClass().getName());
+        list.add("yogurt");
+
+        ArrayList<String> groceries = new ArrayList<>(list);
+        groceries.add("yogurt");
+
+        Syst
+    }
+}
